1ca8f44c8f8543cc4a10aced2e4dfa9a7a3d90ea
Seda Nur Önder <sednrondr@gmail.com>
2022-08-12 23:31:28 +0300

    Initial commit

---
+++ app/src/main/java/com/seda/reminderapp/MainActivity.kt
@@ -0,0 +1,87 @@
+package com.seda.reminderapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
+import androidx.work.workDataOf
+import com.seda.reminderapp.Worker.ReminderWorker
+import com.seda.reminderapp.databinding.ActivityMainBinding
+import java.util.*
+import java.util.concurrent.TimeUnit
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        var chosenYear = 0
+        var chosenMonth = 0
+        var chosenDay = 0
+        var chosenHour = 0
+        var chosenMin = 0
+
+        val today = Calendar.getInstance()
+
+      binding.datePicker.init(today.get(Calendar.YEAR), today.get(Calendar.MONTH),
+            today.get(Calendar.DAY_OF_MONTH)
+        ) {_, year, month, day ->
+          chosenYear = year
+          chosenMonth = month
+          chosenDay = day}
+
+binding.timePicker.setOnTimeChangedListener{_, hour, minute ->
+    chosenHour = hour
+    chosenMin  = minute
+    var am_pm = ""
+    // AM_PM decider logic
+    when {hour == 0 -> { chosenHour += 12
+        am_pm = "AM"
+    }
+        hour == 12 -> am_pm = "PM"
+        hour > 12 -> { chosenHour -= 12
+            am_pm = "PM"
+        }
+        else -> am_pm = "AM"
+    }
+    Log.e("time is","$chosenHour : $minute $am_pm")
+
+}
+
+        binding.setBtn.setOnClickListener {
+            val userSelectedDateTime = Calendar.getInstance()
+
+            userSelectedDateTime.set(chosenYear,chosenMonth,chosenDay,chosenHour,chosenMin)
+
+
+            val todayDateTime = Calendar.getInstance()
+            Log.e("time","${todayDateTime.get(Calendar.HOUR)}")
+            Log.e("time","${todayDateTime.get(Calendar.MINUTE)}")
+            val delayInSeconds =(userSelectedDateTime.get(Calendar.MINUTE))  - (todayDateTime.get(Calendar.MINUTE).toLong())
+            createWorkRequest(binding.editText.text.toString(), delayInSeconds)
+Log.e("delay","$delayInSeconds")
+            // 10
+            Toast.makeText(this, "Reminder set", Toast.LENGTH_SHORT).show()
+
+        }
+
+
+    }
+
+    private fun createWorkRequest(message: String,timeDelayInSeconds: Long  ) {
+        val myWorkRequest = OneTimeWorkRequestBuilder<ReminderWorker>()
+            .setInitialDelay(timeDelayInSeconds, TimeUnit.MINUTES)
+            .setInputData(workDataOf(
+                "title" to "Sepetinde bir şey kaldı",
+                "message" to message,
+            )
+            )
+            .build()
+
+        WorkManager.getInstance(this).enqueue(myWorkRequest)
+    }
+}
---
+++ app/src/main/java/com/seda/reminderapp/Worker/ReminderWorker.kt
@@ -0,0 +1,16 @@
+package com.seda.reminderapp.Worker
+
+import android.content.Context
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+import com.seda.reminderapp.Utils.NotificationHelper
+
+class ReminderWorker (val context: Context, val params: WorkerParameters) : Worker(context, params){
+    override fun doWork(): Result {
+        NotificationHelper(context).createNotification(
+            inputData.getString("title").toString(),
+            inputData.getString("message").toString())
+
+        return Result.success()
+    }
+}
